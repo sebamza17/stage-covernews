032a8d3b5f86efce7a844a23e533315369b6d9fa
Seba Maldonado <sebamza_17@hotmail.com>
2018-06-08 20:13:17 -0300

    Home view, shared styles

---
+++ fe-app/src/app/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+
+// Add both on imports and exports the material module to add it to the application
+@NgModule({
+  imports: [MatButtonModule, MatCheckboxModule],
+  exports: [MatButtonModule, MatCheckboxModule],
+})
+export class MaterialModule {
+}
