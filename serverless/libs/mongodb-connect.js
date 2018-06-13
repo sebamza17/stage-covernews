fe9ae22fdce7f6efbf4bceb1007cd8174e9c2f6b
Claudio A. Marrero <cmarrero01@gmail.com>
2018-06-13 09:03:16 -0300

    Fix export connection to database

--- serverless/libs/mongodb-connect.js
+++ serverless/libs/mongodb-connect.js
@@ -6,7 +6,7 @@ const dbName = 'dictioznews';
 
 let isConnected;
 
-module.exports = connectToDatabase = () => {
+export function connectToDatabase () {
 
     return new Promise((resolve,reject)=>{
 
