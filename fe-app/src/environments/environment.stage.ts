f42dceb6cdda0b4b95641c450641d3293363f9b2
juan ciullini <juanciullini@Air-de-juan.lan>
2018-07-01 20:05:17 -0300

    stage env

---
+++ fe-app/src/environments/environment.stage.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: true,
+  api: {
+    article: "https://jskopm2r2l.execute-api.us-east-1.amazonaws.com/prod",
+    author: "https://29w05vcxd1.execute-api.us-east-1.amazonaws.com/prod",
+    category: "https://docwjx3a3k.execute-api.us-east-1.amazonaws.com/prod",
+    user: "https://cidp4k5462.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  firebase:{
+    apiKey: "AIzaSyD7nmlxbZyPyOoWFrIiKHfS1ryeBWtcu_U",
+    authDomain: "dictioz-603d2.firebaseapp.com",
+    databaseURL: "https://dictioz-603d2.firebaseio.com",
+    projectId: "dictioz-603d2",
+    storageBucket: "dictioz-603d2.appspot.com",
+    messagingSenderId: "459980556882"
+  }
+};
