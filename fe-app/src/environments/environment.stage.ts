3ddebfc11877349551848f2e9b89b836e0c5ead3
juan ciullini <juanciullini@Air-de-juan.lan>
2018-07-02 18:25:25 -0300

    adding stage env for article

--- fe-app/src/environments/environment.stage.ts
+++ fe-app/src/environments/environment.stage.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   api: {
-    article: "https://jskopm2r2l.execute-api.us-east-1.amazonaws.com/prod",
+    article: "https://jskopm2r2l.execute-api.us-east-1.amazonaws.com/stage",
     author: "https://29w05vcxd1.execute-api.us-east-1.amazonaws.com/prod",
     category: "https://docwjx3a3k.execute-api.us-east-1.amazonaws.com/prod",
     user: "https://cidp4k5462.execute-api.us-east-1.amazonaws.com/prod"
