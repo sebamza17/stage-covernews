a4d73b96dd6a986ac8def18d406efc587816c64f
Maribel <maribel.flores@optigo.net>
2018-07-07 09:07:33 -0700

    side-menu WIP

--- fe-app/src/app/shared/material.module.ts
+++ fe-app/src/app/shared/material.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { SideMenuComponent } from './side-menu/side-menu.component';
 
 // Add both on imports and exports the material module to add it to the application
 @NgModule({
   imports: [MatButtonModule, MatCheckboxModule],
   exports: [MatButtonModule, MatCheckboxModule],
-  declarations: [],
+  declarations: [SideMenuComponent],
 })
 export class MaterialModule {
 }
