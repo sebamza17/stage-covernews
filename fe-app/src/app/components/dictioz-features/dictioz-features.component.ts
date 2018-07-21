2cca10dac9793e7ec464af600e1f78787bbb9798
Seba Maldonado <sebamza_17@hotmail.com>
2018-07-21 15:04:37 -0300

    everything merged

--- fe-app/src/app/components/dictioz-features/dictioz-features.component.ts
+++ fe-app/src/app/components/dictioz-features/dictioz-features.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DictiozFeaturesComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
